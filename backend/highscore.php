87372d59122ee3323afc18d101de4e21f58c2384
Dusan Trisic <dusan3sic@pop-os.localdomain>
2024-04-25 12:51:43 +0200

    dodao post za highscore

--- backend/highscore.php
+++ backend/highscore.php
@@ -13,15 +13,42 @@ if ($conn->connect_error) {
 
 $username = "";
 
-if (isset($_GET["username"])){
-    $username = $_GET["username"];
-    
-    $sql = "SELECT * FROM igrac WHERE username='" . $username . "'";
-} else{
-    $sql = "SELECT * from igrac";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+    $highscore = filter_var($_POST['highscore'], FILTER_VALIDATE_INT);
+
+    if ($username !== false && $highscore !== false) {
+        $sql = "INSERT INTO igrac (player_name, highscore) VALUES (?, ?)";
+        $stmt = $conn->prepare($sql);
+
+        $stmt->bind_param("si", $username, $highscore);
+        $result = $stmt->execute();
+        if ($result) {
+            echo "New player added successfully.";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+
+        $stmt->close();
+        $conn->close();
+    } else {
+        echo "Invalid data received.";
+    }
+} if($_SERVER['REQUEST_METHOD'] === 'GET'){
+    if (isset($_GET["username"])){
+        $username = $_GET["username"];
+        
+        $sql = "SELECT * FROM igrac WHERE username='" . $username . "'";
+    } else{
+        $sql = "SELECT * from igrac";
+    }
+    $result = $conn->query($sql);
+}
+else {
+    echo "Invalid request method.";
 }
 
-$result = $conn->query($sql);
+
 
 if ($result->num_rows > 0) {
     $rows = array(); // Initialize an array to store all rows
