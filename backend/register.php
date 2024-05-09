1b89337aab366e0c9578964f41b2fa9282f34000
DusanRd <dule.radivojevic2005@gmail.com>
2024-05-09 13:37:34 +0200

    uradio backend za registraciju

--- backend/register.php
+++ backend/register.php
@@ -3,23 +3,19 @@ $servername = "localhost";
 $password = ""; 
 $database = "igraci";
 
-// Create connection
 $conn = new mysqli($servername, "root", $password, $database);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Get form data
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// Insert data into database
-$sql = "INSERT INTO users (username, email, password) VALUES ('$username', '$email', '$password')";
+$sql = "INSERT INTO igrac (id, username, password, highscore) VALUES (0, '$username', '$password', 0)";
 
 if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
+    echo "Registracija uspesna";
 } else {
     echo "Error: " . $sql . "<br>" . $conn->error;
 }
